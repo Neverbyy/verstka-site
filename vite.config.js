63eef988a46211c00d1ef1026dbd86845e2d59a6
Artem <artem.zaikin04@mail.ru>
2025-04-23 17:07:12 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/verstka/',
+  base: '/verstka-site/',
   server: {
     port: 3000, // ← замени на любой нужный тебе порт
   },
